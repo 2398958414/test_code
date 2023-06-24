adf8b4dada4eb02358f0e0d5a1b30b84f69dbbfc
2398958414 <2398958414@qq.com>
2023-06-24 16:19:19 +0800

    提交代码

---
+++ test_6_19/test_6_19/test.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	//extern 声明外部符号
+//	extern int g_val;
+//	printf("g_val = %d\n", g_val);
+//	return 0;
+//}
+//int main()
+//{
+//	//计算2个数的和
+//	int num_1 = 0;
+//	int num_2 = 0;
+//	//输入数据-使用输入函数scanf
+//	scanf("%d%d", &num_1, &num_2);//取地址符号&
+//	int sum = 0;
+//	sum = num_1 + num_2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+//int a = 3;
+//int main()
+//{
+//	int a = 30;
+//	//全局变量和局部变量的名字建议不要相同-容易误会，产生bug
+//	//当全局变量和局部变量的名字相同时，局部变量优先
+//	printf(" % d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+// 
+//int main()
+//{
+//	printf("hehe\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));//1
+//	printf("%d\n", sizeof(short));//2
+//	printf("%d\n", sizeof(int));//4
+//	printf("%d\n", sizeof(long));//4
+//	printf("%d\n", sizeof(long long));//8
+//	printf("%d\n", sizeof(float));//4
+//	printf("%d\n", sizeof(double));//8
+//
+//	char bc = 'A';  //内存
+//	printf("%c\n",bc); // -- 打印字符格式的数据
+ 	//int age = 22;
+ //   printf("%d\n", age); // -- 打印整型十进制的数据
+  	//float f = 2.1;
+	//printf("%f\n", f);
+	//double d = 2.14;
+	//printf("%lf\n", d);
+	//return 0;
+//}
