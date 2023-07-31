21ecc0be88bab5ff453700aba5cd5d571d867159
2398958414 <2398958414@qq.com>
2023-07-31 22:47:36 +0800

    补充代码

--- test_7_30/test.c
+++ test_7_30/test.c
@@ -32,7 +32,27 @@
 //	         size_t width, 
 //		     int( compare)(const void* e1, const void* e2));
 
-void bubble_sort(void* base, int sz, int width,int(*cmp)())
+int cmp_int(void* e1, void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+
+void Swap(char* elem1, char* elem2,int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp = *elem1;
+		*elem1 = *elem2;
+		*elem2 = tmp;
+		elem1++;
+		elem2++;
+	}
+}
+
+//实现bubble_sort的程序员,是否知道未来要排序的数据类型 - 不知道
+//那程序员也不知道,待比较的两个元素的类型
+void bubble_sort(void* base, int sz, int width,int(*cmp)(void* e1,void* e2))
 {
 	int i = 0;
 	//趟数
@@ -43,9 +63,10 @@ void bubble_sort(void* base, int sz, int width,int(*cmp)())
 		for (j = 0; j < sz - 1 - i; j++)
 		{
 			//两个元素的比较
-			if (cmp() > 0)
+			if (cmp((char*)base+j*width,(char*)base+(j+1)*width) > 0)
 			{
 				//交换
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width,width);
 			}
 		}
 	}
@@ -57,7 +78,7 @@ void test()
 	int sz = sizeof(arr) / sizeof(arr[0]);
 	//使用bubble_sort的程序员知道自己要排序的是什么数据
 	//就应该知道如何比较待排序数组中的元素
-	bubble_sort(arr,sz)
+	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
 }
 
 struct Stu
@@ -65,15 +86,27 @@ struct Stu
 	char name[20];
 	int age;
 };
+
+//int cmp_stu_by_age(void* e1, void* e2)
+//{
+//	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+//}
+#include <string.h>
+int cmp_stu_by_name(void* e1, void* e2)
+{
+	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+}
 void test1()
 {
 	struct Stu s[] = { {"zhangsan",20},{"lisi",18},{"wangwu",22} };
 	int sz = sizeof(s) / sizeof(s[0]);
-	bubble_sort(s,sz)
+	//bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+	bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_name);
 }
 
 int main()
 {
-	test();
+	//test();
+	test1();
 	return 0;
 }
