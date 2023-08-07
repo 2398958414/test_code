ddc52fe2f04ea08ac69af8fdf5659926b1ae2ae1
2398958414 <2398958414@qq.com>
2023-08-07 23:17:46 +0800

    修改代码

--- test_8_6/test.c
+++ test_8_6/test.c
@@ -41,46 +41,83 @@
 //	return 0;
 //}
 
-void reverse(char* left, char* right)
-{
-	while (left<right)
-	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
-		left++;
-		right--;
-	}
-}
+//void reverse(char* left, char* right)
+//{
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//void left_move(char* arr1, int k)
+//{
+//	int len = (int)strlen(arr1);
+//	reverse(arr1,arr1+k-1);//ÄæÐò×ó±ß
+//	reverse(arr1+k,arr1+len-1);//ÄæÐòÓÒ±ß
+//	reverse(arr1,arr1+len-1);//ÄæÐòÕûÌå
+//}
+//int is_left_move(char* s1, char* s2)
+//{
+//	int len = (int)strlen(s1);
+//	int i = 0;
+//	for (i = 0; i < len; i++)
+//	{
+//		left_move(s1, 1);
+//		int ret = strcmp(s1, s2);
+//		if (ret == 0)
+//			return 1;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[] = "cdefab";
+//	int ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
 
-void left_move(char* arr1, int k)
+//abcdef
+//bcdefa
+//cdefab
+//defabc
+//efabcd
+//fabcde
+
+//abcdefabcdef
+int is_left_move(char* str1, char* str2)
 {
-	int len = (int)strlen(arr1);
-	reverse(arr1,arr1+k-1);//ÄæÐò×ó±ß
-	reverse(arr1+k,arr1+len-1);//ÄæÐòÓÒ±ß
-	reverse(arr1,arr1+len-1);//ÄæÐòÕûÌå
-}
-int is_left_move(char* s1, char* s2)
-{
-	int len = (int)strlen(s1);
-	int i = 0;
-	for (i = 0; i < len; i++)
-	{
-		left_move(s1, 1);
-		int ret = strcmp(s1, s2);
-		if (ret == 0)
-			return 1;
-	}
-	return 0;
+	int len1 = (int)strlen(str1);
+	int len2 = (int)strlen(str2);
+	if (len1 != len2)
+		return 0;
+	//1.ÔÚstr1×Ö·û´®ÖÐ×·¼Ó1¸östr1×Ö·û´®
+	//strcat(str1, str1);//err
+	strncat(str1, str1, len1);
+	//2.ÅÐ¶Ïstr2Ö¸ÏòµÄ×Ö·û´®ÊÇ·ñÎªstr1Ö¸ÏòµÄ×Ö·û´®µÄ×Ó´®
+	//strstr - ÕÒ×Ó´®
+	char* ret = strstr(str1, str2);
+	if (ret == NULL)
+		return 0;
+	else
+		return 1;
 }
 int main()
 {
-	char arr1[] = "abcdef";
+	char arr1[30] = "abcdef";
 	char arr2[] = "cdefab";
 	int ret = is_left_move(arr1, arr2);
 	if (ret == 1)
-		printf("YES\n");
+		printf("Yes");
 	else
-		printf("NO\n");
+		printf("No");
 	return 0;
 }
