a72cd37e8848dd20ebc743111eaf8c520d2d0f91
2398958414 <2398958414@qq.com>
2023-09-06 22:40:25 +0800

    补充代码

--- test_9_5/test_9_5/test.c
+++ test_9_5/test_9_5/test.c
@@ -56,14 +56,86 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	//从键盘上读取一行文本信息
+//	char buf[1024] = { 0 };
+//	//fgets(buf, 1024, stdin);//从标准输入流读取
+//	//fputs(buf, stdout);//输出到标准输出流
+//
+//	gets(buf);
+//	puts(buf);
+//	return 0;
+//}
+
+//struct S
+//{
+//	int n;
+//	float score;
+//	char arr[15];
+//};
+////int main()
+////{
+////	struct S s = { 100,3.14f,"Hello World" };
+////	FILE* pf = fopen("test.txt", "w");
+////	if (pf == NULL)
+////	{
+////		return 0;
+////	}
+////	//以格式化的形式写文件
+////	fprintf(pf, "%d %f %s", s.n, s.score, s.arr);
+////	fclose(pf);
+////	pf = NULL;
+////	return 0;
+////}
+//
+//int main()
+//{
+//	struct S s = { 0 };
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//以格式化的形式输入数据
+//	fscanf(pf, "%d %f %s", &(s.n), &(s.score), s.arr);
+//	printf("%d %f %s", s.n, s.score, s.arr);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//struct S
+//{
+//	int n;
+//	float score;
+//	char arr[15];
+//};
+//int main()
+//{
+//	struct S s = { 0 };
+//	fscanf(stdin, "%d %f %s", &(s.n),&(s.score),s.arr);
+//	fprintf(stdout, "%d %.2f %s", s.n, s.score, s.arr);
+//	return 0;
+//}
+
+struct S
+{
+	int n;
+	float score;
+	char arr[15];
+};
 int main()
 {
-	//从键盘上读取一行文本信息
+	struct S s = { 100,3.14f,"abcdef" };
+	struct S tmp = { 0 };
 	char buf[1024] = { 0 };
-	//fgets(buf, 1024, stdin);//从标准输入流读取
-	//fputs(buf, stdout);//输出到标准输出流
+	//把格式化的数据转换成字符串存储到buf
+	sprintf(buf, "%d %f %s", s.n, s.score, s.arr);
+	//printf("%s", buf);
 
-	gets(buf);
-	puts(buf);
+	//从buf中读取格式化的数据到tmp
+	sscanf(buf, "%d %f %s", &(tmp.n), &(tmp.score), tmp.arr);
+	printf("%d %f %s", tmp.n, tmp.score, tmp.arr);
 	return 0;
 }
