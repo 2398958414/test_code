729f20d319e998d4422dcd3edfa3d6e0b664ec96
2398958414 <2398958414@qq.com>
2023-09-10 22:13:12 +0800

    提交代码

---
+++ test_9_10/test_9_10/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+extern int Add(int x, int y);
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int arr[10] = { 0 };
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	int c = Add(a, b);
+	printf("\nc=%d ", c);
+	return 0;
+}
