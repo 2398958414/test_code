46425026afcb7099d4c5c391005c1a665969f78e
2398958414 <2398958414@qq.com>
2023-09-11 20:49:13 +0800

    补充代码

--- test_9_10/test_9_10/test.c
+++ test_9_10/test_9_10/test.c
@@ -4,21 +4,72 @@
 
 extern int Add(int x, int y);
 
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	int c = Add(a, b);
+//	printf("\nc=%d ", c);
+//	return 0;
+//}
+
+//预定义符号
+
+//int main()
+//{
+//	//printf("%s\n", __FILE__);
+//	//printf("%d\n", __LINE__);
+//	//printf("%s\n", __DATE__);
+//	//printf("%s\n", __TIME__);
+//
+//	//写日志文件
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	FILE* pf = fopen("log.txt", "w");
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		fprintf(pf, "file:%s line:%d date:%s time:%s %d\n", __FILE__, __LINE__, __DATE__, __TIME__, i);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+#define MAX 100
+#define STR "Hello World"
+#define reg register
+#define do_forever for(;;)
+
 int main()
 {
-	int a = 10;
-	int b = 20;
-	int arr[10] = { 0 };
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		arr[i] = i;
-	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	int c = Add(a, b);
-	printf("\nc=%d ", c);
+	do_forever;
+	//reg int a;
+	//int max = MAX;
+	//printf("%d\n", max);
+	//printf("%s", STR);
 	return 0;
 }
