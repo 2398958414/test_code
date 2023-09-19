64fbbf0cd635086a4a91e7ce0c39a3163baab844
2398958414 <2398958414@qq.com>
2023-09-19 22:48:57 +0800

    补充代码

--- test_9_16/test_9_16/test.c
+++ test_9_16/test_9_16/test.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
 //char* my_strncpy(char* dest,const char* src,int num)
 //{
@@ -58,60 +59,148 @@
 //	return 0;
 //}
 
-//int main()
+//1,2,3,4,5,1,2,3,4,6
+//1,2,3,4,5,1,2,3,4,
+// 
+//1 1 3 3 5
+//2 2 4 4 6
+// 
+//5^6
+//101
+//110
+//011
+// 
+//5 - A 1 1 3 3
+//6 - B 2 2 4 4
+//分组
+//1.全部数字异或
+//2.找出异或结果中第几位为1,假设为n
+//3.以第n位位0分一组,第n位为1的分一组
+//void find_single_dog(int arr[], int sz, int* pd1, int* pd2)
 //{
-//	int arr[] = { 1,2,3,4,5,1,2,3,4,7 };
-//	int arr_s[10] = { 0 };
-//	int arr_d[10] = { 0 };
-//	int pos_s = 0;
-//	int pos_d = 0;
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	int ret = 0;
-//	int ret_s = 0;
-//	int ret_d= 0;
-//	int count = 0;
 //	int i = 0;
+//	int ret = 0;
+//	//异或
 //	for (i = 0; i < sz; i++)
 //	{
 //		ret ^= arr[i];
 //	}
-//	for (i = 0; i < 32; i++)
+//	//找出异或结果中第几位为1
+//	int pos = 0;
+//	for (pos = 0; pos < 32; pos++)
 //	{
-//		if (((ret >> i) & 1) == 1)
+//		if (((ret >> pos) & 1) == 1)
 //		{
 //			break;
 //		}
-//		count++;
 //	}
 //	for (i = 0; i < sz; i++)
 //	{
-//		if ((arr[i] & (1 << count)) == 1 << count)
+//		if (((arr[i] >> pos) & 1) == 1)
 //		{
-//			arr_s[pos_s] = arr[i];
-//			pos_s++;
+//			*pd1 ^= arr[i];
 //		}
 //		else
 //		{
-//			arr_d[pos_d] = arr[i];
-//			pos_d++;
+//			*pd2 ^= arr[i];
 //		}
 //	}
-//	for (i = 0; i < sz; i++)
-//	{
-//		ret_s ^= arr_s[i];
-//	}
-//	printf("%d ", ret_s);
-//	for (i = 0; i < sz; i++)
-//	{
-//		ret_d ^= arr_d[i];
-//	}
-//	printf("%d ", ret_d);
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,1,2,3,4,6 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int dog1 = 0;
+//	int dog2 = 0;
+//	find_single_dog(arr, sz, &dog1, &dog2);
+//	printf("%d %d", dog1, dog2);
+//
 //	return 0;
 //}
 
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+//1.空指针
+//2.空字符串
+//3.空格
+//4.+ -
+//5.越界
+//6.非数字字符
+
+enum Status
+{
+	VALID,
+	INVALID
+}sta=INVALID;
+int my_atoi(const char* str)
+{
+	assert(str);
+	if (!*str)
+	{
+		return 0;//非法0
+	}
+	while (isspace(*str))
+	{
+		str++;
+	}
+	int flag = 1;
+	if (*str == '+')
+	{
+		flag = 1;
+		str++;
+	}
+	else if (*str == '-')
+	{
+		flag = -1;
+		str++;
+	}
+	long long ret = 0;
+	while (*str)
+	{
+		//越界
+		if (isdigit(*str))
+		{
+			ret = ret * 10 + flag * (*str - '0');
+			if (ret > INT_MAX || ret < INT_MIN)
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			return (int)ret;
+		}
+		str++;
+	}
+	if (!*str)
+		sta = VALID;
+	return (int)ret;
+}
+
 int main()
 {
-	int arr[] = { 1,2,3,4,5,1,2,3,4,6,7 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
+	char arr[] = "123465";
+	//ret=ret*10+1
+	//1
+	int ret = my_atoi(arr);
+	if (sta == INVALID)
+	{
+		printf("非法返回:%d\n", ret);
+	}
+	else
+	{
+		printf("合法返回:%d\n", ret);
+	}
 	return 0;
 }
+
+//int main()
+//{
+//	char a[] = "-100";
+//	char b[] = "123";
+//	int n = 0;
+//	n = my_atoi(a)+my_atoi(b);
+//	printf("n=%d", n);
+//	return 0;
+//}
